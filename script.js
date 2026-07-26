d51e11414fb5c76fd0cdc05c832aa9f4b47faf48
school-in-socks <ujincrab@gmail.com>
2026-07-26 22:26:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+const SHEET_URL = "https://opensheet.elk.sh/1oSAvRDiEZjLkqtsUgakFVGTM39uPEo_aFjRC1rkXBRE/Лист1";
 
-const sold = DATA.sold;
-const target = DATA.target;
-const profit = DATA.profit;
-const p=sold/target*100;
-soldEl.textContent=sold.toLocaleString('ru-RU');
-function soldEl(){}
 
-document.getElementById('sold').textContent=sold.toLocaleString('ru-RU');
-document.getElementById('left').textContent=(target-sold).toLocaleString('ru-RU')+' пирогов';
-document.getElementById('money').textContent=(sold*profit).toLocaleString('ru-RU')+' ₽';
-document.getElementById('percent').textContent=p.toFixed(1)+'% выполнено';
-document.getElementById('fill').style.width=p+'%';
+fetch(SHEET_URL)
+.then(response => response.json())
+.then(data => {
+
+const sold = Number(data[0].sold);
+const target = Number(data[0].target);
+const profit = Number(data[0].profit);
+
+
+document.getElementById('sold').textContent =
+sold.toLocaleString('ru-RU');
+
+
+document.getElementById('left').textContent =
+(target - sold).toLocaleString('ru-RU') + " пирогов";
+
+
+document.getElementById('money').textContent =
+(sold * profit).toLocaleString('ru-RU') + " ₽";
+
+
+const percent = sold / target * 100;
+
+
+document.getElementById('percent').textContent =
+percent.toFixed(1) + "% выполнено";
+
+
+document.getElementById('fill').style.width =
+percent + "%";
+
+
+})
+.catch(error => {
+console.log("Ошибка загрузки таблицы:", error);
+});
